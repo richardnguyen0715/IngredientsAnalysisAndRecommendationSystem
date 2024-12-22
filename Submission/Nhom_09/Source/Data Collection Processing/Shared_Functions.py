62b11350dcc98d89082fc729efb9ba72986e4556
Richard Nguyen <richardnguyen0715@gmail.com>
2024-12-22 23:22:43 +0700

    Tuong_Update_Submission

---
+++ Submission/Nhom_09/Source/Data Collection Processing/Shared_Functions.py
@@ -0,0 +1,79 @@
+from Libraries_Used import *
+
+def write_to_file(filepath: str, content , how : str):
+    """
+    Writes content to a file based on the specified method.
+
+    Parameters:
+        filepath (str): The path to the file to write to.
+        content (str or list): The content to write to the file. 
+            - If `how` is 'w_b_str', content should be a string.
+            - If `how` is 'w_b_element', content should be a list.
+        how (str): The method to write the content.
+            - 'w_b_str': Write the entire content as a single string, suitable for HTML files.
+            - 'w_b_element': Write each element of the list on a new line, suitable for TXT files.
+
+    Returns:
+        None: This function does not return anything.
+    """
+    with open(filepath, 'w', encoding="utf-8") as file:
+        try:
+            if how == 'w_b_str':
+                file.write(content)
+            elif how == 'w_b_element':
+                for line in content: 
+                    file.write(str(line) + '\n')
+        except Exception as err:
+            print(f'{how} is not one of the supported values.')
+
+def read_from_file(filepath: str, how : str):
+    """
+    Reads a file based on the specified method.
+
+    Parameters:
+        filepath (str): The path to the file to read.
+        how (str): The method to read the file.
+            - 'r_b_str': Read the entire file as a single string. Recommended for HTML files.
+            - 'r_b_line': Read the file line-by-line, returning a list of lines. Recommended for TXT files.
+
+    Returns:
+        Union[str, List[str]]: 
+            - If `how` is 'r_b_str', returns the file content as a single string.
+            - If `how` is 'r_b_line', returns the file content as a list of lines.
+
+    Raises:
+        ValueError: If `how` is not one of the supported values.
+    """
+    with open(filepath, 'r', encoding="utf-8") as file:
+        try :
+            if how == 'r_b_str':
+                content = ''
+                content = file.read()
+                return content
+            elif how == 'r_b_line':
+                content = []
+                content = file.read().splitlines()  
+                return content
+        except Exception as err:
+            print(f'{how} is not one of the supported values.')
+
+def create_file_path():
+    current_directory = Path(__file__)
+    parent_directory = current_directory.parent.parent
+    # Testing the read_input_file method
+    filename = str(parent_directory)
+    return filename
+
+def clean_text(text):
+    text = text.replace("\n", "").replace(" \n", " ").replace("\n ", " ").strip()
+    text = ' '.join(text.split())
+    return text
+
+
+def save_dataset(filename, df):
+    dataset_path = create_file_path() + '\\Assert'
+    cleaned_data = {title: clean_text(ingredients) for title, ingredients in df.items()}
+    df_return = pd.DataFrame(list(cleaned_data.items()), columns=['Title', 'Ingredients'])
+    df_return.to_csv(dataset_path + '\\' + filename, index=False, encoding="utf-8")
+    
+    
